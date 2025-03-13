3ed88be23e2cf611bab8c796b19e20e071751fe8
ADEX-1 <adesamad1234@gmail.com>
2025-03-13 23:05:28 +0100

    added cmake

---
+++ backend/build/CMakeFiles/MyProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyProject.dir/source/_server.cpp.o"
+  "CMakeFiles/MyProject.dir/source/_server.cpp.o.d"
+  "MyProject"
+  "MyProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
